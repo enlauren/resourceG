01ed767507e866e352e5c106a2b09025e28d5e2f
enlauren <enlau101@gmail.com>
2022-03-20 21:45:20 +0000

    cleanup

--- expressionTree.js
+++ expressionTree.js
@@ -1,25 +1,23 @@
 const assert = require('assert');
 
-const Op = (arg1, arg2) => ({
-    Addition: {
+const Op = () => ({
+    Add: {
         Symbol: '+',
-        Operation: arg1+arg2
+        Operation: (arg1, arg2) => arg1+arg2
     },
-    Subtraction: {
+    Subtract: {
         Symbol: '-',
-        Operation: arg1-arg2
+        Operation: (arg1, arg2) => arg1-arg2
     },
-    Division:  {
+    Divide:  {
         Symbol: '÷',
-        Operation: arg1/arg2
+        Operation: (arg1, arg2) => arg1/arg2
     },
-    Multiplication: {
+    Multiply: {
         Symbol: 'x',
-        Operation: arg1*arg2
+        Operation: (arg1, arg2) => arg1*arg2
     }
 })
-
-
 
 const ExpressionTreeArithmetic = ({left, operator, right}) => {
     const toString = function () {
@@ -31,7 +29,7 @@ const ExpressionTreeArithmetic = ({left, operator, right}) => {
     const result = function () {
         const setLeft = typeof left === 'object' ? left.result() : left;
         const setRight = typeof right === 'object' ? right.result() : right;
-        return Op(setLeft, setRight).Operation
+        return operator.Operation(setLeft, setRight)
     };
 
     return {toString, result};
@@ -42,15 +40,14 @@ const Node = ({left, operator, right}) => ExpressionTreeArithmetic({left, operat
 
 const tree = ExpressionTreeArithmetic({
     left: Node({
-        left: 7, operator: Op().Addition, right: Node({
+        left: 7, operator: Op().Add, right: Node({
             left: Node({
-                left: 3, operator: Op().Subtraction, right: 2
-            }), operator: Op().Multiplication, right: 5,
+                left: 3, operator: Op().Subtract, right: 2
+            }), operator: Op().Multiply, right: 5,
         })
     }),
-    operator: Op().Division, right: 6
+    operator: Op().Divide, right: 6
 });
 
-console.log(tree.toString())
 assert.strictEqual("((7 + ((3 - 2) x 5)) ÷ 6)", tree.toString());
-// assert.strictEqual(2, tree.result());
+assert.strictEqual(2, tree.result());
